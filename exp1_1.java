c1582828648189e0a197534af3e172befbc9c0ea
Keshvi-08 <keshvisiroya2006@gmail.com>
2024-08-16 12:21:17 +0530

    Create exp1_1.java

---
+++ exp1_1.java
@@ -0,0 +1,11 @@
+// Write a program that declares one integer variable called var1. Give value 10
+// to this variable and then, using one println() statement, display the value on
+// the screen like this: “10 is the value of var1.”
+  public class exp2{
+    
+    public static void main(String[] args) {
+        int var1=10;
+        System.out.println(var1+ "is the value of var1");
+        System.out.println("23CS095");
+    }
+}
