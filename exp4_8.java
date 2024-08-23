05223b571faa3e64e836a39cf77b8a392d766269
Keshvi-08 <keshvisiroya2006@gmail.com>
2024-08-23 10:04:22 +0530

    Create exp4_8.java

---
+++ exp4_8.java
@@ -0,0 +1,15 @@
+// Write a java program which shows importing of classes from other user define
+// packages
+
+public class exp4_8 {
+
+    public static void main(String[] args) {
+        
+    }
+
+    public static void data(){
+        System.out.println("hello, how are you?");
+    }
+}
+
+
